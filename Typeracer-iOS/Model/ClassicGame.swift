e3367fb862bc6e7e1a062d84b9d3e223aa5df0b7
Nurbekismagulov <160107092@stu.sdu.edu.kz>
2018-08-02 13:31:40 +0600

    refactoring

---
+++ Typeracer-iOS/Model/ClassicGame.swift
@@ -0,0 +1,9 @@
+//
+//  ClassicGame.swift
+//  Typeracer-iOS
+//
+//  Created by Nurbek Ismagulov on 01.08.2018.
+//  Copyright © 2018 theSmartest. All rights reserved.
+//
+
+import Foundation
---
+++ Typeracer-iOS/View/ClassicTextView.swift
@@ -0,0 +1,21 @@
+//
+//  ClassicTextView.swift
+//  Typeracer-iOS
+//
+//  Created by Nurbek Ismagulov on 02.08.2018.
+//  Copyright © 2018 theSmartest. All rights reserved.
+//
+
+import UIKit
+
+class ClassicTextView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
